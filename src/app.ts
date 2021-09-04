f9232106d63fc011245588a46e398b348a4200b1
TanTruong <tptan12345@gmail.com>
2021-09-04 23:10:23 +0700

    add Procfile

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const app = express();
-
+const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-app.listen(80, () => console.log('Server running'));
+app.listen(port, () => console.log('Server running:', port));
